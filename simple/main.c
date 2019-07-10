6a71cdbd3668056b19958c46903a1941d1501410
Gabriel-Andrew Pollo Guilbert <gabrielpolloguilbert@hotmail.com>
2019-07-10 17:51:15 -0400

    Add some logging to simple executable

--- simple/main.c
+++ simple/main.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <time.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
 #define TRACEPOINT_DEFINE
 #include "tp_provider.h"
 
+#define FORK_COUNT 1000
+
+#define VA_ARGS(...) , ##__VA_ARGS__
+#define log(fmt, ...) 							\
+	do { 								\
+		time_t ts; 						\
+		ts = time(NULL); 					\
+		char* date = asctime(localtime(&ts)); 			\
+		date[strlen(date) - 1] = '\0';				\
+		printf("[%s] " fmt "\n", date VA_ARGS(__VA_ARGS__));	\
+	} while (0);
+
 int main()
 {
 	tracepoint(fork_test, process_spawned, getpid());
 
-	for(int i = 0; i < 1; i++) {
+	for(int i = 0; i < FORK_COUNT; i++) {
 		pid_t pid = fork();
 		if (pid < 0) {
 			perror("fork");
@@ -19,10 +32,12 @@ int main()
 
 		if (pid) {
 			tracepoint(fork_test, child_spawned, getpid(), pid);
+			log("waiting for child...");
 			pid_t child = wait(NULL);
 			if (child < 0) {
 				perror("wait");
 			}
+			log("done waiting for child %u", child);
 			tracepoint(fork_test, child_terminated, getpid(), pid);	
 		} else {
 			tracepoint(fork_test, process_spawned, getpid());
