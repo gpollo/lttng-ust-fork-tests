1f1a0d2ed0bb5fc2f0092f944da6aec5f3941623
Gabriel-Andrew Pollo Guilbert <gabrielpolloguilbert@hotmail.com>
2019-07-11 14:09:29 -0400

    Small changes

--- simple/run.sh
+++ simple/run.sh
@@ -4,6 +4,11 @@ EXECUTABLE=$(basename "$PWD")
 
 export LTTNG_UST_REGISTER_TIMEOUT=-1
 export LD_PRELOAD=liblttng-ust-fork.so
-./$EXECUTABLE
+./$EXECUTABLE &
+./$EXECUTABLE &
+./$EXECUTABLE &
+./$EXECUTABLE &
+./$EXECUTABLE &
 unset LTTNG_UST_REGISTER_TIMEOUT
 unset LD_PRELOAD
+wait $(jobs -p)
