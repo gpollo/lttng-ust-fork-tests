1bd163bc231ba92a0569d6eeda84a0cb5285c705
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2019-07-25 11:24:43 -0400

    Don't put simple test in background

--- simple/run.sh
+++ simple/run.sh
@@ -2,13 +2,11 @@
 
 EXECUTABLE=$(basename "$PWD")
 
-export LTTNG_UST_REGISTER_TIMEOUT=-1
-export LD_PRELOAD=liblttng-ust-fork.so
-./$EXECUTABLE &
-./$EXECUTABLE &
-./$EXECUTABLE &
-./$EXECUTABLE &
-./$EXECUTABLE &
-unset LTTNG_UST_REGISTER_TIMEOUT
-unset LD_PRELOAD
-wait $(jobs -p)
+function run_bin() {
+	export LTTNG_UST_REGISTER_TIMEOUT=-1
+	export LD_PRELOAD=liblttng-ust-fork.so
+	./$EXECUTABLE 2> run.log > run.log
+	unset LD_PRELOAD
+}
+
+time run_bin
