261a5aea65e32a41fcfd474617094a0b050239f3
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2019-07-25 17:33:50 -0400

    Run time command on same-pid test

--- same-pid/run.sh
+++ same-pid/run.sh
@@ -2,8 +2,19 @@
 
 EXECUTABLE=$(basename "$PWD")
 
-export LTTNG_UST_REGISTER_TIMEOUT=-1
-export LD_PRELOAD=liblttng-ust-fork.so
-./$EXECUTABLE
-unset LTTNG_UST_REGISTER_TIMEOUT
-unset LD_PRELOAD
+function run_bin() {
+	export LTTNG_UST_DEBUG=1
+	export LTTNG_UST_REGISTER_TIMEOUT=-1
+	export LTTNG_UST_ALLOW_BLOCKING=1
+	export LD_PRELOAD=liblttng-ust-fork.so
+
+	./$EXECUTABLE 2> run.log > run.log
+
+	unset LTTNG_UST_DEBUG
+	unset LTTNG_UST_REGISTER_TIMEOUT
+	unset LTTNG_UST_ALLOW_BLOCKING
+	unset LD_PRELOAD
+}
+
+time run_bin
+
