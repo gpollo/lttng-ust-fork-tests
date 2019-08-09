f85f541b8c1b2e2e68ff3c957c55606bc001ec32
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2019-08-09 16:10:25 -0400

    Kill ./run.sh processes in clean.sh

--- clean.sh
+++ clean.sh
@@ -4,6 +4,7 @@ sudo kill -KILL $(ps aux | grep -i lttng       | grep -v grep | grep -v vim | aw
 sudo kill -KILL $(ps aux | grep -i ./simple    | grep -v grep | grep -v vim | awk '{print $2}')
 sudo kill -KILL $(ps aux | grep -i ./same-pid  | grep -v grep | grep -v vim | awk '{print $2}')
 sudo kill -KILL $(ps aux | grep -i ./recursive | grep -v grep | grep -v vim | awk '{print $2}')
+sudo kill -KILL $(ps aux | grep -i ./run.sh    | grep -v grep | grep -v vim | awk '{print $2}')
 
 sudo rm -rf ~/.lttng
 sudo rm -rf ~/lttng-traces
